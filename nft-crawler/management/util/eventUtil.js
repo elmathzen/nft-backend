513c1115bf5e40ebe47c7e9723ebde3d739597f5
Peter Nahm <hwnahm@gmail.com>
2021-12-22 18:15:16 +0900

    Block duplicate insert

--- nft-crawler/management/util/eventUtil.js
+++ nft-crawler/management/util/eventUtil.js
@@ -4,18 +4,21 @@ const models = require('../models');
 
 export const createTrade = async function (event) {
     console.log('add trade =====>', event);
-    // create send history
-    let trade = new models.NftTrades();
-    trade.nft = '0x977cc5eaa3adC6225d2cB9cb04c68dda6219BD54';
-    trade.tokenId = event.returnValues.tokenId;
-    trade.seller = event.returnValues.seller;
-    // trade.price = await getFormattedAmount(event.returnValues.price);
-    trade.price = event.returnValues.price;
-    trade.onSale = true;
-    trade.txHash1 = event.transactionHash;
+    const trade = await models.NftTrades.findToken('0x977cc5eaa3adC6225d2cB9cb04c68dda6219BD54', event.returnValues.tokenId);
+    console.log('exit trade =====>', trade);
+    if (!trade) {
+        // create Trade
+        let trade = new models.NftTrades();
+        trade.nft = '0x977cc5eaa3adC6225d2cB9cb04c68dda6219BD54';
+        trade.tokenId = event.returnValues.tokenId;
+        trade.seller = event.returnValues.seller;
+        // trade.price = await getFormattedAmount(event.returnValues.price);
+        trade.price = event.returnValues.price;
+        trade.onSale = true;
+        trade.txHash1 = event.transactionHash;
 
-    trade = await trade.save();
-    return trade;
+        await trade.save();
+    }
 }
 
 export const updateTrade = async function (event) {
@@ -41,6 +44,7 @@ export const deleteTrade = async function (event) {
     if (trade) {
         trade.onSale = false;
         trade.txHash3 = event.transactionHash;
+
         await trade.save();
     }
 }
